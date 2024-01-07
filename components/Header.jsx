5b2d4f3f32ea4f04dd85f320b7389c47f5801923
VelizarDabov <velizardabov@gmail.com>
2024-01-07 11:38:48 +0200

    setup and header

---
+++ components/Header.jsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+import React from "react";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
+const Header = () => {
+  return (
+    <div className="">
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+        {/* left */}
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2880px-Instagram_logo.svg.png "
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-10  lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://1000logos.net/wp-content/uploads/2017/02/insta-logo.png"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        {/* middle */}
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md  ">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black"
+              placeholder="Search"
+              type="text"
+            />
+          </div>
+        </div>
+
+        {/* 
+    right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+            <div className="absolute  -top-2 -right-1 text-xs  w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+              3
+            </div>
+          </div>
+
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+
+          <img
+            src="https://scontent.fsof9-1.fna.fbcdn.net/v/t39.30808-6/348610572_3128974100580964_386632649417346818_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=ZjyTxxQWzlIAX_2bRzE&_nc_ht=scontent.fsof9-1.fna&oh=00_AfAdhAGh0mqH5ZptsnS7n-zP1cAWhEgvWrBZ_znlR4IcaA&oe=659EBAE7"
+            alt="profile pic"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+          "upload.wikimedia.org",
+          '1000logos.net',
+        ],
+    }
+}
+
+module.exports = nextConfig

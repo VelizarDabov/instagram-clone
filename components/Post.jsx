09e702d6f81e6cd26556d59fa22b1aba623eb89b
VelizarDabov <velizardabov@gmail.com>
2024-01-08 17:32:23 +0200

    add like comment

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import {
   BookmarkIcon,
   ChatIcon,
@@ -8,7 +9,72 @@ import {
   PaperAirplaneIcon,
 } from "@heroicons/react/outline";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "./firebase";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import Moment from "react-moment";
 const Post = ({ img, username, id, userImg, caption }) => {
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(
+    () =>
+      setHasLiked(likes.findIndex((like) => like.id === user[0]?.uid) !== -1),
+    [likes]
+  );
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "posts", id, "likes"), (snapshot) =>
+        setLikes(snapshot.docs)
+      )[(db, id)]
+  );
+
+  const likePost = async () => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, "posts", id, "likes", user[0].uid));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", user[0].uid), {
+        username: user[0].displayName,
+      });
+    }
+  };
+  const user = useAuthState(auth);
+  const sendComment = async (e) => {
+    e.preventDefault();
+    const commentToSend = comment;
+    setComment("");
+
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: user[0].displayName,
+      userImage: user[0].photoURL,
+      timestamp: serverTimestamp(),
+    });
+  };
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db]
+  );
   return (
     <div className="bg-white my-7 border rounded-sm">
       {/* header */}
@@ -25,7 +91,12 @@ const Post = ({ img, username, id, userImg, caption }) => {
       {/* buttons */}
       <div className="flex justify-between px-4 pt-4">
         <div className="flex space-x-4">
-          <HeartIcon className="btn" />
+          {hasLiked ? (
+            <HeartIconFilled onClick={likePost} className="btn text-red-400" />
+          ) : (
+            <HeartIcon onClick={likePost} className="btn" />
+          )}
+          <HeartIcon onClick={likePost} className="btn" />
           <ChatIcon className="btn" />
           <PaperAirplaneIcon className="btn" />
         </div>
@@ -35,20 +106,53 @@ const Post = ({ img, username, id, userImg, caption }) => {
       {/* caption */}
       <div>
         <p className="p-5 truncate">
-            <span className="font-bold mr-1">
-                {username}
-            </span>
-            {caption}
+          {likes.length > 0 && (<p className="font-bold mb-1">{likes.length} likes</p>)}
+          <span className="font-bold mr-1">{username}</span>
+          {caption}
         </p>
       </div>
       {/* comments */}
-      <form className="flex items-center p-4 ">
-        <EmojiHappyIcon className="btn"/>
-        <input type="text" placeholder="Add a comment..." className="border-none flex-1 outline-none"/>
-        <button className="font-semibold text-blue-400">
+      {comments.length > 0 && (
+        <div className=" ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+          {comments.map((comment) => (
+            <div key={comment.id} className="flex items-center space-x-2 mb-3">
+              <img
+                src={comment.data().userImage}
+                alt="comment img"
+                className="h-7 rounded-full"
+              />
+              <p className="text-sm flex-1 ">
+                <span className="font-bold">{comment.data().username}</span>{" "}
+                {comment.data().comment}
+              </p>
+              <Moment className="pr-5 text-xs" fromNow>
+                {comment.data().timestamp?.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
+      {user && (
+        <form className="flex items-center p-4 ">
+          <EmojiHappyIcon className="btn" />
+          <input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Add a comment..."
+            className="border-none flex-1 outline-none"
+          />
+          <button
+            type="submit"
+            disabled={!comment}
+            onClick={sendComment}
+            className="font-semibold text-blue-400 cursor-pointer"
+          >
             Post
-        </button>
-      </form>
+          </button>
+        </form>
+      )}
+
       {/* input box */}
     </div>
   );
